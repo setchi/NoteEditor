fdca5b8bb9f25c8edec115fd63ad3a7da5116290
setchi <ri_xer@yahoo.co.jp>
2015-07-03 01:17:35 +0900

    Fix NullReferenceException

--- Assets/Scripts/NotesEditor/CanvasEvents.cs
+++ Assets/Scripts/NotesEditor/CanvasEvents.cs
@@ -8,13 +8,14 @@ public class CanvasEvents : MonoBehaviour
     public Subject<Vector3> ScrollPadOnMouseEnterObservable = new Subject<Vector3>();
     public Subject<Vector3> ScrollPadOnMouseDownObservable = new Subject<Vector3>();
     public Subject<Vector3> ScrollPadOnMouseExitObservable = new Subject<Vector3>();
-    public IObservable<float> MouseScrollWheelObservable;
+    public Subject<float> MouseScrollWheelObservable = new Subject<float>();
 
     void Awake()
     {
-        MouseScrollWheelObservable = this.UpdateAsObservable()
+        this.UpdateAsObservable()
             .Select(_ => Input.GetAxis("Mouse ScrollWheel"))
-            .Where(delta => delta != 0);
+            .Where(delta => delta != 0)
+            .Subscribe(MouseScrollWheelObservable.OnNext);
     }
 
     public void ScrollPadOnMouseDown()
