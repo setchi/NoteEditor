748e8e14d0f50c8c806620c8f10f90fa6ccd06f8
setchi <ri_xer@yahoo.co.jp>
2015-07-11 18:23:26 +0900

    Fix bad performance of AuxiliaryLineRenderer

--- Assets/Scripts/UI/AuxiliaryLineRendererer.cs
+++ Assets/Scripts/UI/AuxiliaryLineRendererer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class AuxiliaryLineRendererer : MonoBehaviour
@@ -18,72 +20,110 @@ public class AuxiliaryLineRendererer : MonoBehaviour
     void Awake()
     {
         model = NotesEditorModel.Instance;
-    }
 
-    void LateUpdate()
-    {
-        if (model.Audio == null || model.Audio.clip == null)
-            return;
+        var beatSamples = new int[1];
+        var beatLines = new Line[1];
+        var blockLines = new Line[1];
+        var cachedZeroSamplePosX = -1f;
+        var cachedCanvasWidth = 0f;
+        var TMP_MAX_BLOCK = 5;
 
-        var unitBeatSamples = Mathf.FloorToInt(model.Audio.clip.frequency * 60f / model.BPM.Value);
-        var beatNum = model.LPB.Value * Mathf.CeilToInt(model.Audio.clip.samples / (float)unitBeatSamples);
-        var beatSamples = Enumerable.Range(0, beatNum)
-            .Select(i => i * unitBeatSamples / model.LPB.Value)
-            .ToArray();
+        this.LateUpdateAsObservable()
+            .Where(_ => model.Audio != null && model.Audio.clip != null)
+            .Subscribe(_ => {
 
-
-        var beatLines = beatSamples
-            .Select(x => model.SamplesToScreenPositionX(x))
-            .Select((x, i) => new Line(
-                new Vector3(x, 140, 0),
-                new Vector3(x, -140, 0),
-                i % model.LPB.Value == 0 ? mainBeatLineColor : subBeatLineColor))
-            .ToArray();
+                var unitBeatSamples = Mathf.FloorToInt(model.Audio.clip.frequency * 60f / model.BPM.Value);
+                var beatNum = model.LPB.Value * Mathf.CeilToInt(model.Audio.clip.samples / (float)unitBeatSamples);
+                float check_time = Time.realtimeSinceStartup;
 
 
-        var blockLines = Enumerable.Range(0, 5)
-            .Select(i => model.BlockNumToScreenPositionY(i))
-            .Select(i => i + Screen.height * 0.5f)
-            .Select((y, i) => new Line(
-                model.ScreenToCanvasPosition(new Vector3(0, y, 0)),
-                model.ScreenToCanvasPosition(new Vector3(Screen.width, y, 0)),
-                blockLineColor))
-            .ToArray();
+                if (beatSamples.Length != beatNum || cachedCanvasWidth != model.CanvasWidth.Value)
+                {
+                    beatSamples = Enumerable.Range(0, beatNum)
+                        .Select(i => i * unitBeatSamples / model.LPB.Value)
+                        .ToArray();
+
+                    beatLines = beatSamples
+                        .Select(x => model.SamplesToScreenPositionX(x))
+                        .Select((x, i) => new Line(
+                            new Vector3(x, 140, 0),
+                            new Vector3(x, -140, 0),
+                            i % model.LPB.Value == 0 ? mainBeatLineColor : subBeatLineColor))
+                        .ToArray();
+
+                    cachedZeroSamplePosX = beatLines[0].start.x;
+                    cachedCanvasWidth = model.CanvasWidth.Value;
+                }
+                else
+                {
+                    float currentX = model.SamplesToScreenPositionX(0);
+                    float diffX = currentX - cachedZeroSamplePosX;
+
+                    for (int i = 0; i < beatNum; i++)
+                    {
+                        beatLines[i].end.x = (beatLines[i].start.x += diffX);
+                        beatLines[i].color = i % model.LPB.Value == 0 ? mainBeatLineColor : subBeatLineColor;
+                    }
+
+                    cachedZeroSamplePosX = currentX;
+                }
 
 
-        // Highlighting closest line to mouse pointer
-        if (model.IsMouseOverNotesRegion.Value)
-        {
-            var mouseX = model.ScreenToCanvasPosition(Input.mousePosition).x;
-            var closestLineIndex = GetClosestLineIndex(beatLines, c => Mathf.Abs(c.start.x - mouseX));
-            var closestBeatLine = beatLines[closestLineIndex];
+                if (blockLines.Length != TMP_MAX_BLOCK)
+                {
+                    blockLines = Enumerable.Range(0, TMP_MAX_BLOCK)
+                        .Select(i => model.BlockNumToScreenPositionY(i))
+                        .Select(i => i + Screen.height * 0.5f)
+                        .Select((y, i) => new Line(
+                            model.ScreenToCanvasPosition(new Vector3(0, y, 0)),
+                            model.ScreenToCanvasPosition(new Vector3(Screen.width, y, 0)),
+                            blockLineColor))
+                        .ToArray();
+                }
+                else
+                {
+                    for (int i = 0; i < TMP_MAX_BLOCK; i++)
+                    {
+                        blockLines[i].color = blockLineColor;
+                    }
+                }
 
-            var mouseY = model.ScreenToCanvasPosition(Input.mousePosition).y;
-            var closestBlockLindex = GetClosestLineIndex(blockLines, c => Mathf.Abs(c.start.y - mouseY));
-            var closestBlockLine = blockLines[closestBlockLindex];
 
-            var distance = Vector2.Distance(
-                new Vector2(closestBeatLine.start.x, closestBlockLine.start.y),
-                new Vector2(mouseX, mouseY));
+                // Highlighting closest line to mouse pointer
+                if (model.IsMouseOverNotesRegion.Value)
+                {
+                    var mouseX = model.ScreenToCanvasPosition(Input.mousePosition).x;
+                    var closestLineIndex = GetClosestLineIndex(beatLines, c => Mathf.Abs(c.start.x - mouseX));
+                    var closestBeatLine = beatLines[closestLineIndex];
 
-            var threshold = Mathf.Min(
-                Mathf.Abs(model.BlockNumToScreenPositionY(0) - model.BlockNumToScreenPositionY(1)),
-                Mathf.Abs(model.SamplesToScreenPositionX(beatSamples[0]) - model.SamplesToScreenPositionX(beatSamples[1]))) / 3f;
+                    var mouseY = model.ScreenToCanvasPosition(Input.mousePosition).y;
+                    var closestBlockLindex = GetClosestLineIndex(blockLines, c => Mathf.Abs(c.start.y - mouseY));
+                    var closestBlockLine = blockLines[closestBlockLindex];
 
-            if (distance < threshold)
-            {
-                closestBlockLine.color = highlightColor;
-                closestBeatLine.color = highlightColor;
-                model.ClosestNotePosition.Value = new NotePosition(model.BPM.Value, model.LPB.Value, closestLineIndex, closestBlockLindex);
-            }
-            else
-            {
-                model.ClosestNotePosition.Value = new NotePosition(-1, -1, -1, -1);
-            }
-        }
+                    var distance = Vector2.Distance(
+                        new Vector2(closestBeatLine.start.x, closestBlockLine.start.y),
+                        new Vector2(mouseX, mouseY));
 
-        GLLineRenderer.RenderLines("beats", beatLines);
-        GLLineRenderer.RenderLines("blocks", blockLines);
+                    var threshold = Mathf.Min(
+                        Mathf.Abs(model.BlockNumToScreenPositionY(0) - model.BlockNumToScreenPositionY(1)),
+                        Mathf.Abs(model.SamplesToScreenPositionX(beatSamples[0]) - model.SamplesToScreenPositionX(beatSamples[1]))) / 3f;
+
+                    if (distance < threshold)
+                    {
+                        closestBlockLine.color = highlightColor;
+                        closestBeatLine.color = highlightColor;
+                        model.ClosestNotePosition.Value = new NotePosition(model.BPM.Value, model.LPB.Value, closestLineIndex, closestBlockLindex);
+                    }
+                    else
+                    {
+                        model.ClosestNotePosition.Value = new NotePosition(-1, -1, -1, -1);
+                    }
+                }
+
+                GLLineRenderer.RenderLines("beats", beatLines);
+                GLLineRenderer.RenderLines("blocks", blockLines);
+
+            });
     }
 
     int GetClosestLineIndex(Line[] lines, Func<Line, float> calcDistance)
